6c6cd12386cfb25f8fac88477685b541c595d36e
iVandalise <36800115+iVandalise@users.noreply.github.com>
2023-07-14 17:46:03 +0100

    Functional Date Search Query

--- src/components/ZonePrediction/CustomDatePicker.js
+++ src/components/ZonePrediction/CustomDatePicker.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const CustomDatePicker = () => {
+const CustomDatePicker = ({onDateChange}) => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
+    onDateChange(date); // Call the onDateChange prop
   };
 
   return (
