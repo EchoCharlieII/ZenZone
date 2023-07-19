1db4f5ce4dab2ce769af66f03cde26f07a48072f
rookieeew <rookieeew@gmail.com>
2023-07-19 22:52:19 +0100

    render map api

--- map_api/urls.py
+++ map_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('render-map', views.render_map)
 ]
--- predict_api/urls.py
+++ predict_api/urls.py
@@ -5,7 +5,4 @@ from . import views
 
 urlpatterns = [
     path('predict-by-id', views.predict_by_id),
-    
-    # The following pattern is for testing purposes
-    path('dummy_test', views.my_view_function)
 ]
