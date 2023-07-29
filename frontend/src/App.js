841a1a01a383403248f8218451aaec8fe7663396
monsieurcherry <sakura.hayashi.97@gmail.com>
2023-07-29 10:34:18 +0200

    Landing Page + Potential FourSquare Integration?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/'; // Yi debug record
+// import './App.css';
+// import Navbar from './components/Navbar/'; // Yi debug record
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
 import Map from './pages/map';
-//import MyMap from './components/MyMap';
-import MapRoute from './pages/route';
 import Home from './pages/index'; // Yi debug record
 
  
@@ -19,7 +15,6 @@ function App() {
                     <Route exact path='/' element={<Home />} /> 
                     {/* Yi debug record */}
                     <Route path='/map' element={<Map />} />
-                    <Route path='/route' element={<MapRoute />} />
                 </Routes>
             </Router>     
         </div>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,23 +1,106 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css'
+import React, { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import './Home.css';
+import ScrollIcon from './ScrollIcon';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const section1 = useRef(null);
+    const section2 = useRef(null);
+    const [section2Opacity, setSection2Opacity] = useState(0);
+
+    // Scroll position and window height variables
+    let scrollPos = 0;
+    let windowH = 0;
+
+    // Function to update elements based on the scroll position
+    const updateElements = () => {
+        // Check if refs are assigned
+        if (!section1.current || !section2.current) {
+            return;
+        }
+
+        const progress = scrollPos / windowH;
+        console.log("progress:", progress);  // <- for debugging
+        gsap.to(section1.current, { opacity: 1 - progress });
+        gsap.to(section2.current, { opacity: progress });
+
+        setSection2Opacity(progress);
+        console.log("section2 opacity:", section2.current.style.opacity);  // <- for debugging
+    };
+
+    useEffect(() => {
+        // Disable the default scroll
+        document.body.style.overflow = 'hidden';
+
+        // Get the window height
+        windowH = window.innerHeight;
+
+        // Listen to the wheel event
+        window.addEventListener('wheel', (e) => {
+            // Update the scroll position
+            scrollPos += e.deltaY;
+
+            // Make sure the scroll position is not outside the boundaries
+            if (scrollPos < 0) scrollPos = 0;
+            else if (scrollPos > windowH) scrollPos = windowH;
+
+            // Update the elements
+            updateElements();
+        });
+
+        // Update the elements based on the initial scroll position
+        updateElements();
+
+        // Set initial opacity of section 2 to 0
+        gsap.set(section2.current, { opacity: 0 });
+
+        // Listen to the transitionend event
+        section2.current.addEventListener('transitionend', (e) => {
+            // If the transitioned property is 'opacity' and the new opacity is '1'...
+            if (e.propertyName === 'opacity' && e.target.style.opacity === '1') {
+                // Find all .section2-description elements and trigger their animations
+                document.querySelectorAll('.section2-description').forEach((desc) => {
+                    desc.style.animationPlayState = 'running';
+                });
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        // Get the ScrollIcon by its class or id
+        const scrollIcon = document.querySelector("#mouse-scroll");
+
+        // If the ScrollIcon exists...
+        if (scrollIcon) {
+            // If section2Opacity is 1, hide the ScrollIcon. Otherwise, show it.
+            scrollIcon.style.display = section2Opacity >= 0.99 ? 'none' : 'block';
+        }
+    }, [section2Opacity]);
+
     return (
-    <div className="home-container">
-        <div className="animated-background">
-            <div className="falling-leaves"></div>
+        <div className="home-container">
+            <div ref={section1} id="section1">
+                <div className="content">
+                    <h1 className="home-title">Find Your Zen</h1>
+                    <p className="home-description">Ready to uncover NYC's secret havens with ZenZone?</p>
+                </div>
+            </div>
+            <div ref={section2} id="section2">
+                <div className="content">
+                    <p className="section2-description line1">Embark on a journey of tranquility.</p>
+                    <p className="section2-description line2">Discover the serene spots and hidden gems of New York City.</p>
+                    <p className="section2-description line3">Unravel the Unseen, Embrace the Calm.</p>
+                </div>
+            </div>
+            {section2Opacity >= 0.99 && (
+                <Link to="/map" style={{ textDecoration: 'none' }}>
+                    <button className="home-button">Explore Now</button>
+                </Link>
+            )}
+            <ScrollIcon />
         </div>
-        <div className="content"> 
-            <h1 className="home-title">Welcome to ZenZone</h1>
-            <p className="home-description">Discover serene spots and hidden gems in nature</p>
-            {/* Add logo. */}
-            <img className="home-image" src="/path/to/logo.jpg" alt="ZenZone logo" />
-            {/* Add routing. */}
-            <button className="home-button">Explore Now</button>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
