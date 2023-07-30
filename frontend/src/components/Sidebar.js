f1c7524b266c94f11df1e92581efa69ca0aeb77d
monsieurcherry <sakura.hayashi.97@gmail.com>
2023-07-30 15:57:03 +0200

    Fixed Sidebar bug

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,44 +8,47 @@ import logo from './logo_text.svg';
 import '../pages/Map.css';
 
 function Sidebar() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const [showDateSelector, setShowDateSelector] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
-  const toggleDateSelector = () => {
-    setShowDateSelector(!showDateSelector);
-  };
-
-  return (
-    <div id="app">
-      <ProSidebar 
-        className="pro-sidebar" 
-        collapsed={!isSidebarOpen} 
-        width={isSidebarOpen ? '310px' : '80px'}
-      >
-        <Menu>
-          <MenuItem
-            icon={<MenuOutlinedIcon />}
-            onClick={toggleSidebar}
-          >
-            <img src={logo} alt="Logo" style={{ width: '150px', marginTop: '5px' }}/>
-          </MenuItem>
-          <MenuItem onClick={isSidebarOpen ? toggleDateSelector : undefined} icon={<RouteOutlinedIcon />}>
-            Route Planner
-          </MenuItem>
-          {showDateSelector && 
-            <div className="date-selector-container">
-              <DateSelector />
-            </div>
-          }
-          <MenuItem icon={<DiamondOutlinedIcon />}>Hidden Gems</MenuItem>
-        </Menu>
-      </ProSidebar>
-    </div>
-  );
-}
-
-export default Sidebar;
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+    const [showDateSelector, setShowDateSelector] = useState(false);
+  
+    const toggleSidebar = () => {
+      setSidebarOpen(!isSidebarOpen);
+      if (isSidebarOpen) {   // If the sidebar is currently open
+        setShowDateSelector(false);   // Hide the date selector
+      }
+    };
+  
+    const toggleDateSelector = () => {
+      setShowDateSelector(!showDateSelector);
+    };
+  
+    return (
+      <div id="app">
+        <ProSidebar 
+          className="pro-sidebar" 
+          collapsed={!isSidebarOpen} 
+          width={isSidebarOpen ? '310px' : '80px'}
+        >
+          <Menu>
+            <MenuItem
+              icon={<MenuOutlinedIcon />}
+              onClick={toggleSidebar}
+            >
+              <img src={logo} alt="Logo" style={{ width: '150px', marginTop: '5px' }}/>
+            </MenuItem>
+            <MenuItem onClick={isSidebarOpen ? toggleDateSelector : undefined} icon={<RouteOutlinedIcon />}>
+              Route Planner
+            </MenuItem>
+            {showDateSelector && 
+              <div className="date-selector-container">
+                <DateSelector />
+              </div>
+            }
+            <MenuItem icon={<DiamondOutlinedIcon />}>Hidden Gems</MenuItem>
+          </Menu>
+        </ProSidebar>
+      </div>
+    );
+  }
+  
+  export default Sidebar;
