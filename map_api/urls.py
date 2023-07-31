023366b8643cd44872bfca9dcb12c61e903756d0
rookieeew <113436260+rookieeew@users.noreply.github.com>
2023-07-31 12:45:07 +0100

    best path

--- map_api/urls.py
+++ map_api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('render-map', views.render_map)
+    path('render-map', views.render_map),
+    path('best-path', views.best_path),
 ]
