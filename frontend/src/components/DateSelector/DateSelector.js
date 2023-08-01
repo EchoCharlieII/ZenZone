4c415bfe59ca50bb32a64d5a886ba9ed595f3eab
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-08-01 15:27:18 +0200

    Dateselector for current time, after appearing

--- frontend/src/components/DateSelector/DateSelector.js
+++ frontend/src/components/DateSelector/DateSelector.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomDatePicker from './CustomDatePicker';
 import SubmitButton from './SubmitButton';
 import ApiService from '../../services/ApiService';
@@ -6,7 +6,21 @@ import './DateSelector.css';
 
 const DateSelector = ({ onDateSubmit }) => {
   const [selectedDate, setSelectedDate] = useState(null);
-  const [mapData, setMapData] = useState([]);
+
+  useEffect(() => {
+    // This code runs after the component is mounted
+    if(selectedDate == null){
+      ApiService.submitDate(new Date())
+        .then((response) => {
+          console.log("Api Response:", response.data);
+          onDateSubmit(response.data); // Pass the data to the parent component
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -14,10 +28,10 @@ const DateSelector = ({ onDateSubmit }) => {
 
   const handleSubmit = () => {
     if (selectedDate) {
+      console.log(selectedDate);
       ApiService.submitDate(selectedDate)
       .then((response) => {
         console.log("Api Response:", response.data);
-        setMapData(response.data); // Set received data to the state variable
         onDateSubmit(response.data); // Pass the data to the parent component
       })
       .catch((error) => {
