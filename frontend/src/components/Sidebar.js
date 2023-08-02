00920d6091e096c1b2b51176d5fabd29a50be5f3
monsieurcherry <sakura.hayashi.97@gmail.com>
2023-08-02 23:34:58 +0200

    Open sidebar from any icon

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,34 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Sidebar as ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import RouteOutlinedIcon from '@mui/icons-material/RouteOutlined';
-import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
-import LocalCafeOutlinedIcon from '@mui/icons-material/LocalCafeOutlined';
-import MuseumOutlinedIcon from '@mui/icons-material/MuseumOutlined';
-import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
-import ParkOutlinedIcon from '@mui/icons-material/ParkOutlined';
+import RouteOutlinedIcon from "@mui/icons-material/RouteOutlined";
+import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
+import LocalCafeOutlinedIcon from "@mui/icons-material/LocalCafeOutlined";
+import MuseumOutlinedIcon from "@mui/icons-material/MuseumOutlined";
+import LocalLibraryOutlinedIcon from "@mui/icons-material/LocalLibraryOutlined";
+import ParkOutlinedIcon from "@mui/icons-material/ParkOutlined";
 import DateSelector from "./DateSelector/DateSelector";
-import logo from './logo_text.svg';
-import '../pages/Map.css';
-import opencage from 'opencage-api-client';
-import Autosuggest from 'react-autosuggest';
+import logo from "./logo_text.svg";
+import "../pages/Map.css";
+import opencage from "opencage-api-client";
+import Autosuggest from "react-autosuggest";
 
-function Sidebar({ onLocationsSelected, setMapData }) {
+export default function Sidebar({ onLocationsSelected, setMapData }) {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [showDateSelector, setShowDateSelector] = useState(false);
   const [showGemItems, setShowGemItems] = useState(false);
 
   // For the Starting Location
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
   // For the Destination Location
-  const [valueDestination, setValueDestination] = useState('');
+  const [valueDestination, setValueDestination] = useState("");
   const [suggestionsDestination, setSuggestionsDestination] = useState([]);
 
   const [startLocation, setStartLocation] = useState(null);
   const [endLocation, setEndLocation] = useState(null);
-  
+
   const onSuggestionSelected = (event, { suggestion }) => {
     setStartLocation(suggestion.latlng);
   };
@@ -36,11 +36,10 @@ function Sidebar({ onLocationsSelected, setMapData }) {
   const onSuggestionSelectedDestination = (event, { suggestion }) => {
     setEndLocation(suggestion.latlng);
   };
-  
 
   const fetch = ({ value }) => {
     opencage
-      .geocode({ key: '378bbab421b943cc95ef067c6295c57a', q: value })
+      .geocode({ key: "378bbab421b943cc95ef067c6295c57a", q: value })
       .then((response) => {
         const { results } = response;
         setSuggestions(
@@ -51,13 +50,13 @@ function Sidebar({ onLocationsSelected, setMapData }) {
         );
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
   };
 
   const fetchDestination = ({ value }) => {
     opencage
-      .geocode({ key: '378bbab421b943cc95ef067c6295c57a', q: value })
+      .geocode({ key: "378bbab421b943cc95ef067c6295c57a", q: value })
       .then((response) => {
         const { results } = response;
         setSuggestionsDestination(
@@ -68,12 +67,12 @@ function Sidebar({ onLocationsSelected, setMapData }) {
         );
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
   };
 
   const inputProps = {
-    placeholder: 'Search a location...',
+    placeholder: "Search a location...",
     value,
     onChange: (_, { newValue }) => {
       setValue(newValue);
@@ -81,7 +80,7 @@ function Sidebar({ onLocationsSelected, setMapData }) {
   };
 
   const inputPropsDestination = {
-    placeholder: 'Search a destination...',
+    placeholder: "Search a destination...",
     value: valueDestination,
     onChange: (_, { newValue }) => {
       setValueDestination(newValue);
@@ -128,7 +127,7 @@ function Sidebar({ onLocationsSelected, setMapData }) {
 
   const toggleGemItems = () => {
     setShowGemItems(!showGemItems);
-  };  
+  };
 
   useEffect(() => {
     if (startLocation && endLocation) {
@@ -137,18 +136,20 @@ function Sidebar({ onLocationsSelected, setMapData }) {
     }
   }, [startLocation, endLocation, onLocationsSelected]);
 
-  
-
   return (
     <div id="app">
       <ProSidebar
         className="pro-sidebar"
         collapsed={!isSidebarOpen}
-        width={isSidebarOpen ? '380px' : '80px'}
+        width={isSidebarOpen ? "380px" : "80px"}
       >
         <Menu>
           <MenuItem onClick={toggleSidebar} icon={<MenuOutlinedIcon />}>
-            <img src={logo} alt="Logo" style={{ width: '150px', marginTop: '5px' }} />
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "150px", marginTop: "5px" }}
+            />
           </MenuItem>
           <MenuItem
             onClick={() => {
@@ -162,6 +163,7 @@ function Sidebar({ onLocationsSelected, setMapData }) {
 
           {showDateSelector && (
             <div className="autosuggest-form">
+              <p style={{ fontSize: "16px" }}>Select starting and ending points:</p>
               <Autosuggest
                 suggestions={suggestions}
                 onSuggestionsFetchRequested={onSuggestionsFetchRequested}
@@ -172,34 +174,39 @@ function Sidebar({ onLocationsSelected, setMapData }) {
                 onSuggestionSelected={onSuggestionSelected}
                 theme={{
                   suggestionsList: {
-                    listStyleType: 'none', // Removes bullet points
+                    listStyleType: "none", // Removes bullet points
                   },
                   suggestion: {
-                    fontSize: '14px', // Sets font size to 14px
-                  }
+                    fontSize: "14px", // Sets font size to 14px
+                  },
                 }}
               />
               <Autosuggest
                 suggestions={suggestionsDestination}
-                onSuggestionsFetchRequested={onSuggestionsFetchRequestedDestination}
-                onSuggestionsClearRequested={onSuggestionsClearRequestedDestination}
+                onSuggestionsFetchRequested={
+                  onSuggestionsFetchRequestedDestination
+                }
+                onSuggestionsClearRequested={
+                  onSuggestionsClearRequestedDestination
+                }
                 getSuggestionValue={getSuggestionValueDestination}
                 renderSuggestion={renderSuggestionDestination}
                 inputProps={inputPropsDestination}
                 onSuggestionSelected={onSuggestionSelectedDestination}
                 theme={{
                   suggestionsList: {
-                    listStyleType: 'none', // Removes bullet points
-                    marginLeft: '-35px'
+                    listStyleType: "none", // Removes bullet points
+                    marginLeft: "-35px",
                   },
                   suggestion: {
-                    fontSize: '13px', // Sets font size to 14px
-                  }
+                    fontSize: "13px", // Sets font size to 14px
+                  },
                 }}
               />
               <div className="date-selector-container">
-                <DateSelector onDateSubmit={setMapData}/>
+                <DateSelector onDateSubmit={setMapData} />
               </div>
+              <button onClick={null}>Clear</button> {/*  TODO: Clear button */}
             </div>
           )}
           <MenuItem 
@@ -221,10 +228,7 @@ function Sidebar({ onLocationsSelected, setMapData }) {
               >
                 Cafes for Serenity Seekers
               </MenuItem>
-              <MenuItem
-                className="gem-menu-item"
-                icon={<MuseumOutlinedIcon />}
-              >
+              <MenuItem className="gem-menu-item" icon={<MuseumOutlinedIcon />}>
                 Museums for Quiet Contemplation
               </MenuItem>
               <MenuItem
@@ -233,10 +237,7 @@ function Sidebar({ onLocationsSelected, setMapData }) {
               >
                 Libraries for Focused Study
               </MenuItem>
-              <MenuItem
-                className="gem-menu-item"
-                icon={<ParkOutlinedIcon />}
-              >
+              <MenuItem className="gem-menu-item" icon={<ParkOutlinedIcon />}>
                 Parks for Idyllic Breaks
               </MenuItem>
             </>
@@ -246,5 +247,3 @@ function Sidebar({ onLocationsSelected, setMapData }) {
     </div>
   );
 }
-
-export default Sidebar;
