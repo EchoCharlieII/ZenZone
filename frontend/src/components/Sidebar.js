ff5cac29cb6890f054b211b344b9f84db2860c27
monsieurcherry <sakura.hayashi.97@gmail.com>
2023-08-03 01:42:41 +0200

    Restrict location suggestions to NY.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -37,40 +37,55 @@ export default function Sidebar({ onLocationsSelected, setMapData }) {
     setEndLocation(suggestion.latlng);
   };
 
-  const fetch = ({ value }) => {
-    opencage
-      .geocode({ key: "378bbab421b943cc95ef067c6295c57a", q: value })
-      .then((response) => {
-        const { results } = response;
-        setSuggestions(
-          results.map((result) => ({
-            name: result.formatted,
-            latlng: [result.geometry.lat, result.geometry.lng],
-          }))
-        );
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
-  const fetchDestination = ({ value }) => {
-    opencage
-      .geocode({ key: "378bbab421b943cc95ef067c6295c57a", q: value })
-      .then((response) => {
-        const { results } = response;
-        setSuggestionsDestination(
-          results.map((result) => ({
-            name: result.formatted,
-            latlng: [result.geometry.lat, result.geometry.lng],
-          }))
-        );
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
+    // Restrict location suggestions to New York.
+    const fetch = ({ value }) => {
+      opencage
+        .geocode({ 
+          key: "378bbab421b943cc95ef067c6295c57a", 
+          q: value, 
+          bounds: [
+            [-74.25909, 40.477399], // southwest corner
+            [-73.700272, 40.917577] // northeast corner
+          ] 
+        })
+        .then((response) => {
+          const { results } = response;
+          setSuggestions(
+            results.map((result) => ({
+              name: result.formatted,
+              latlng: [result.geometry.lat, result.geometry.lng],
+            }))
+          );
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+  
+    const fetchDestination = ({ value }) => {
+      opencage
+        .geocode({ 
+          key: "378bbab421b943cc95ef067c6295c57a", 
+          q: value, 
+          bounds: [
+            [-74.25909, 40.477399], // southwest corner
+            [-73.700272, 40.917577] // northeast corner
+          ] 
+        })
+        .then((response) => {
+          const { results } = response;
+          setSuggestionsDestination(
+            results.map((result) => ({
+              name: result.formatted,
+              latlng: [result.geometry.lat, result.geometry.lng],
+            }))
+          );
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+  
   const inputProps = {
     placeholder: "Search a location...",
     value,
